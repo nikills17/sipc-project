9c8dde65177a93eece1029a648cd4a9182e0d535
RohanSingh <rohanstomar11@gmail.com>
2023-03-12 17:14:39 +0530

    test

--- src/Screens/Dashboard.js
+++ src/Screens/Dashboard.js
@@ -1,12 +1,10 @@
-import { View, Text } from 'react-native'
+import {View, Text} from 'react-native';
 import React from 'react';
 
-import { MMKV } from 'react-native-mmkv'
+import {MMKV} from 'react-native-mmkv';
 export const storage = new MMKV();
 
-
-const Dashboard = ({ navigation }) => {
- 
+const Dashboard = ({navigation}) => {
   const jsonUser = storage.getString('user');
   const user = JSON.parse(jsonUser);
 
@@ -14,7 +12,7 @@ const Dashboard = ({ navigation }) => {
     <View>
       <Text>Dashboard</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
